1c6ff4c5f52a22505fdf0def8ca3237c14e673ff
alexandresilvestri <airtontunni@gmail.com>
2025-12-08 10:07:20 -0800

    api connect to database

---
+++ backend/src/database/knexfile.ts
@@ -0,0 +1,33 @@
+import type { Knex } from 'knex'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '../.env' })
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || 'construction-manager',
+      port: Number(process.env.DB_PORT) || 5432,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations',
+      extension: 'ts'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+}
+
+export default config
