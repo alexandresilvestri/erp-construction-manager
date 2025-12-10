1ee556c2312080ba8112c34e7974df37e30ec873
alexandresilvestri <airtontunni@gmail.com>
2025-12-09 17:02:15 -0800

    create migrate for users table

--- backend/src/database/migrations/20251209235103_create-users.ts
+++ backend/src/database/migrations/20251209235103_create-users.ts
@@ -1,19 +1,18 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
 
-    return knex.schema.createTable('users', (table) => {
-        table.uuid('id', { primaryKey: true }).defaultTo(knex.raw('uuid_generate_v4()'));
-        table.string('email').notNullable().unique()
-        table.string('password').notNullable() 
-        table.timestamps(true, true)
-    })
+  return knex.schema.createTable('users', (table) => {
+    table
+      .uuid('id', { primaryKey: true })
+      .defaultTo(knex.raw('uuid_generate_v4()'))
+    table.string('email').notNullable().unique()
+    table.string('password').notNullable()
+    table.timestamps(true, true)
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTable('users')
+  return knex.schema.dropTable('users')
 }
-
