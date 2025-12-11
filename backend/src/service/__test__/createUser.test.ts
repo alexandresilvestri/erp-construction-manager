9b98f381f8a96d5540f6d879eb9114842a814726
gaspar <airtontunni@gmail.com>
2025-12-10 21:27:53 -0300

    createUser service

--- backend/src/service/__test__/createUser.test.ts
+++ backend/src/service/__test__/createUser.test.ts
@@ -1,8 +1,23 @@
 import { describe, it, expect } from 'vitest'
+import { createUser, CreateUserParams } from '../createUser'
 
-describe('todo', () => {
-  it('returns nothing', () => {
-    const todo = 'todo'
-    expect(todo).toEqual('todo')
+describe('create user', () => {
+  const createUserParams: CreateUserParams = {
+    email: 'alexandre@mail.com',
+    password: 'Alexa123',
+  }
+
+  describe('when the user doesnt not exists', () => {
+    it('returns an user', async () => {
+      const createdUser = await createUser(createUserParams)
+
+      expect(createdUser).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          email: createUserParams.email,
+          passwordHash: expect.any(String),
+        })
+      )
+    })
   })
 })
